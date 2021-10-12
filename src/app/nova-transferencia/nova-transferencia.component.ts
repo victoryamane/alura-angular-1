db057365ea6fd2b33c22733ad4752f40e803688e
Victor Yamane <yamanevictor@gmail.com>
2021-10-12 14:39:31 -0300

    Aula 03

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 
 @Component({
@@ -7,13 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss']
 
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor: number;
   destino: number;
 
-  transferir(){
+  transferir() {
     console.log('Solicitada nova transferência');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
   }
 }
